f149c9fe65df62bc174e9b6532c6cd8e503b58b7
Jakub <=>
2022-07-08 00:25:41 +0200

    fruit pick up

--- array/pick_fruits.py
+++ array/pick_fruits.py
@@ -2,50 +2,36 @@ from typing import List
 from math import inf
 
 def pick_fruits(fruits: List[int]) -> int:
-    fruits_seq = []
-    curr = fruits[0]
-    seq = 0
-    for i in range(len(fruits)):
-        if fruits[i] == curr:
-            seq += 1
-        else:
-            fruits_seq.append((curr, seq))
-            curr = fruits[i]
-            seq = 1
-
-        if i == len(fruits) - 1:
-            fruits_seq.append((curr, seq))
-    
     beg = 0
     end = 0
     total = 0
     window_values = {}
     max_seq = -inf
 
-    while end < len(fruits_seq)    :
-        fruit, count = fruits_seq[end]
+    while end < len(fruits)    :
+        fruit = fruits[end]
 
         if fruit in window_values:
-            window_values[fruit] += count
-            total += count
+            window_values[fruit] += 1
+            total += 1
             max_seq = max(max_seq, total)
             end += 1
         else:
             if len(window_values) == 2:
                 while beg <= end and len(window_values) >= 2:
-                    fruit, count = fruits_seq[beg]
-                    window_values[fruit] -= count
-                    total -= count
+                    fruit = fruits[beg]
+                    window_values[fruit] -= 1
+                    total -= 1
 
                     if window_values[fruit] == 0:
                         del window_values[fruit]
                     beg += 1
             else:
-                window_values[fruit] = count
-                total += count
+                window_values[fruit] = 1
+                total += 1
                 end += 1
                 max_seq = max(max_seq, total)
-    return max_seq, fruits_seq[beg:end + 1]
+    return max_seq, fruits[beg:end + 1]
 
 
 fruits = [1,2,3,2,2]
