c6e758f8a8d481453623993ea896a5953c6486fc
Jakub <=>
2022-07-09 10:09:00 +0200

    add parent field to TreeNode

--- trees/Node.py
+++ trees/Node.py
@@ -1,5 +1,6 @@
 class Node():
-    def __init__(self, val : int, left = None, right = None) -> None:
+    def __init__(self, val : int, left = None, right = None, parent = None) -> None:
         self.left : Node = left
         self.right : Node = right
+        self.parent : Node = parent
         self.val = val
