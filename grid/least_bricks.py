c287fac49093e9b071d9e84546e27f9da6158a01
Jakub <=>
2022-07-10 12:10:59 +0200

    least bricks

--- grid/least_bricks.py
+++ grid/least_bricks.py
@@ -4,6 +4,8 @@ from typing import List
 def least_bricks(wall: List[List[int]]) -> int:
     spots = {}
 
+    max_spots = 0
+
     for layer in wall:
         spot = 0
         for brick_index in range(len(layer) - 1):
@@ -15,13 +17,9 @@ def least_bricks(wall: List[List[int]]) -> int:
                 spots[spot] = 0
             spots[spot] += 1
 
-    max_spots = -math.inf
+            max_spots = max(max_spots, spots[spot])
 
-    for spot in spots.keys():
-        max_spots = max(max_spots, spots[spot])
-
-
-    return len(wall) if len(spots) == 0 else len(wall) - max_spots
+    return len(wall) - max_spots
     
 wall = [
     [1,2,2,1],
